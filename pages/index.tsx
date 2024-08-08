9bee8d1b9d7587a04a7c392503da5cbce7dbb7f0
Tim Dobson <hello@tdobson.net>
2024-08-08 03:01:38 +0100

    fix: correct import and usage of Layout component in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import { Layout } from '../components/Layout/Layout';
-import { HomePage } from '../components/HomePage/HomePage';
-
 import Layout from '../components/Layout/Layout';
 import HomePage from '../components/HomePage/HomePage';
 
@@ -8,15 +5,6 @@ export default function Index() {
     return (
         <Layout>
             <HomePage />
-                                </ThemeIcon>
-                            }
-                        >
-                            <List.Item>I’m currently the chair of The Climbing Clan and The Caving Crew, BMC and BCA affiliated climbing and caving clubs that I founded.</List.Item>
-                            <List.Item>Through my work with these clubs, I've developed several software products to assist with club operations. Most notably, I've created neoClan, a second-generation membership check-in system.</List.Item>
-                            <List.Item>I’m currently teaching myself React with the Mantine framework and Next.js, as well as exploring react-query and advanced git workflows.</List.Item>
-                            <List.Item>I’m keen on opportunities for freelance work and development. If you have an exciting project, I'd love to collaborate!</List.Item>
-                            <List.Item>Ask me about Linux administration, rock climbing, and caving.</List.Item>
-                            <List.Item>Fun fact: I have a wonderful son named James.</List.Item>
                         </List>
                     </Box>
                 </div>
