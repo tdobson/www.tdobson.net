852d1f50909b5e6743bcb23b682e1938d91e6faf
Tim Dobson <hello@tdobson.net>
2024-08-08 03:09:48 +0100

    feat: update timeline component with new content and center alignment

--- components/Timeline/Timeline.tsx
+++ components/Timeline/Timeline.tsx
@@ -1,32 +1,114 @@
-import { Timeline, Text, ThemeIcon } from '@mantine/core';
+import { Timeline, Text, ThemeIcon, Avatar } from '@mantine/core';
 import { IconGitBranch, IconGitCommit, IconGitPullRequest, IconMessageDots } from '@tabler/icons-react';
 
 export function TimDobsonTimeline() {
   return (
-    <Timeline active={3} bulletSize={24} lineWidth={2}>
-      <Timeline.Item bullet={<IconGitBranch size={12} />} title="The Climbing Clan and The Caving Crew">
-        <Text c="dimmed" size="sm">Currently the chair of The Climbing Clan and The Caving Crew, BMC and BCA affiliated climbing and caving clubs that I founded.</Text>
-      </Timeline.Item>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Timeline active={2} lineWidth={6} bulletSize={40}>
+        <Timeline.Item
+          title="Junior Javascript Developer at XEBIT LIMITED (March 2022 - Present)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">I write Javascript integrating with Google Sheets and WordPress to help businesses automate their processes.</Text>
+        </Timeline.Item>
 
-      <Timeline.Item bullet={<IconGitCommit size={12} />} title="Software Development">
-        <Text c="dimmed" size="sm">Developed several software products to assist with club operations, most notably neoClan, a second-generation membership check-in system.</Text>
-      </Timeline.Item>
+        <Timeline.Item
+          title="Chair of The Climbing Clan (November 2017 - Present)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">Founded a climbing group and developed software products to support club operations.</Text>
+        </Timeline.Item>
 
-      <Timeline.Item title="Current Learning" bullet={<IconGitPullRequest size={12} />}>
-        <Text c="dimmed" size="sm">Currently teaching myself React with the Mantine framework and Next.js, as well as exploring react-query and advanced git workflows.</Text>
-      </Timeline.Item>
+        <Timeline.Item
+          title="Chair of The Caving Crew (January 2020 - Present)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">Founded a caving group with a very similar setup and structure to the Climbing Clan.</Text>
+        </Timeline.Item>
 
-      <Timeline.Item title="Freelance Opportunities" bullet={<IconMessageDots size={12} />}>
-        <Text c="dimmed" size="sm">Keen on opportunities for freelance work and development. If you have an exciting project, I'd love to collaborate!</Text>
-      </Timeline.Item>
+        <Timeline.Item
+          title="Technical Sales Consultant at Migrating Dragons (April 2018 - March 2022)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">Supported technology companies in building and executing sales strategies.</Text>
+        </Timeline.Item>
 
-      <Timeline.Item title="Expertise" bullet={<ThemeIcon size={22} radius="xl"><IconGitBranch size={12} /></ThemeIcon>}>
-        <Text c="dimmed" size="sm">Ask me about Linux administration, rock climbing, and caving.</Text>
-      </Timeline.Item>
+        <Timeline.Item
+          title="Customer Success Manager at Brightbox (October 2018 - October 2019)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">Helped developers test our platform and gather feedback.</Text>
+        </Timeline.Item>
 
-      <Timeline.Item title="Personal" bullet={<ThemeIcon size={22} radius="xl"><IconGitBranch size={12} /></ThemeIcon>}>
-        <Text c="dimmed" size="sm">Fun fact: I have a wonderful son named James.</Text>
-      </Timeline.Item>
-    </Timeline>
+        <Timeline.Item
+          title="User Research Specialist at Guardian Angel (February 2019 - July 2019)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">Conducted user research to improve a free tool for families following the death of a loved one.</Text>
+        </Timeline.Item>
+
+        <Timeline.Item
+          title="Senior Account Manager at Formisimo (July 2017 - April 2018)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">Onboarded enterprise customers onto our SaaS platform.</Text>
+        </Timeline.Item>
+
+        <Timeline.Item
+          title="Head of Sales & Account Management at Bytemark Hosting (November 2010 - September 2016)"
+          bullet={
+            <Avatar
+              size={22}
+              radius="xl"
+              src="https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
+            />
+          }
+        >
+          <Text c="dimmed" size="sm">Managed customer relationships and sales, leading to significant revenue growth.</Text>
+        </Timeline.Item>
+      </Timeline>
+    </div>
   );
 }
