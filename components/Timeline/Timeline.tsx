743ea09d912bbc9d694d9caf739f960860799465
Tim Dobson <hello@tdobson.net>
2024-08-08 19:39:27 +0100

    style: Remove brackets from date segments and change styling from dimmed to gray

--- components/Timeline/Timeline.tsx
+++ components/Timeline/Timeline.tsx
@@ -24,7 +24,7 @@ export function TimDobsonTimeline() {
             />
           }
         >
-          <Text c="dimmed" size="sm">(March 2022 - Present)</Text>
+          <Text c="gray" size="sm">March 2022 - Present</Text>
           <Text c="dimmed" size="sm">I write Javascript integrating with Google Sheets and WordPress to help businesses automate their processes.</Text>
         </Timeline.Item>
 
@@ -40,7 +40,7 @@ export function TimDobsonTimeline() {
             />
           }
         >
-          <Text c="dimmed" size="sm">(January 2020 - Present)</Text>
+          <Text c="gray" size="sm">January 2020 - Present</Text>
           <Text c="dimmed" size="sm">Founded a caving group with a very similar setup and structure to the Climbing Clan.</Text>
         </Timeline.Item>
 
@@ -54,7 +54,7 @@ export function TimDobsonTimeline() {
                   />
               }
           >
-              <Text c="dimmed" size="sm">(November 2017 - September 2024)</Text>
+              <Text c="gray" size="sm">November 2017 - September 2024</Text>
               <Text c="dimmed" size="sm">Founded a climbing group, developed software products to support club operations and grew it to 200+ members.</Text>
           </Timeline.Item>
 
@@ -68,7 +68,7 @@ export function TimDobsonTimeline() {
             />
           }
         >
-          <Text c="dimmed" size="sm">(April 2018 - March 2022)</Text>
+          <Text c="gray" size="sm">April 2018 - March 2022</Text>
           <Text c="dimmed" size="sm">Supported technology companies in building and executing sales strategies.</Text>
         </Timeline.Item>
 
@@ -82,7 +82,7 @@ export function TimDobsonTimeline() {
             />
           }
         >
-          <Text c="dimmed" size="sm">(October 2018 - October 2019)</Text>
+          <Text c="gray" size="sm">October 2018 - October 2019</Text>
           <Text c="dimmed" size="sm">Helped developers test our platform and gather feedback.</Text>
         </Timeline.Item>
 
@@ -96,7 +96,7 @@ export function TimDobsonTimeline() {
             />
           }
         >
-          <Text c="dimmed" size="sm">(February 2019 - July 2019)</Text>
+          <Text c="gray" size="sm">February 2019 - July 2019</Text>
           <Text c="dimmed" size="sm">Conducted user research to improve a free tool for families following the death of a loved one.</Text>
         </Timeline.Item>
 
@@ -110,7 +110,7 @@ export function TimDobsonTimeline() {
             />
           }
         >
-          <Text c="dimmed" size="sm">(July 2017 - April 2018)</Text>
+          <Text c="gray" size="sm">July 2017 - April 2018</Text>
           <Text c="dimmed" size="sm">Onboarded enterprise customers onto our SaaS platform.</Text>
         </Timeline.Item>
 
@@ -124,7 +124,7 @@ export function TimDobsonTimeline() {
             />
           }
         >
-          <Text c="dimmed" size="sm">(November 2010 - September 2016)</Text>
+          <Text c="gray" size="sm">November 2010 - September 2016</Text>
           <Text c="dimmed" size="sm">Managed customer relationships and sales, leading to significant revenue growth.</Text>
         </Timeline.Item>
       </Timeline>
