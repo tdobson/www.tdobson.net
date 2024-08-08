c6147642a89af9644842c5409818e96f09701f54
Tim Dobson <hello@tdobson.net>
2024-08-08 15:56:37 +0100

    feat: add vertical spacing between carousel and timeline component

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ export default function Index() {
             </Head>
             <HeroSection />
             <TimDobsonTimeline />
+            <Space h="xl" />
             <TestimonialsCarousel />
         </Layout>
     );
