ad6a036b48c4ddf62fe3dbe7a249a454c0c5f341
Tim Dobson <hello@tdobson.net>
2024-08-08 08:51:51 +0100

    fix: move global CSS import to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import '../components/Timeline/Timeline.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
