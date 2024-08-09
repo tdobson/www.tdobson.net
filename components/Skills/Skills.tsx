3498c14221e7cc5af9c702a73269595aaac1a062
Tim Dobson <hello@tdobson.net>
2024-08-09 12:47:50 +0100

    refactor: remove description line from Skills component

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -3,22 +3,22 @@ import { IconBrandJavascript, IconBrandPhp, IconBrandHtml5, IconBrandCss3, IconB
 import styles from './Skills.module.css';
 
 const skills = [
-    { icon: IconBrandJavascript, title: 'JavaScript', description: 'Proficient' },
-    { icon: IconBrandPhp, title: 'PHP', description: 'Proficient' },
-    { icon: IconBrandHtml5, title: 'HTML', description: 'Proficient' },
-    { icon: IconBrandCss3, title: 'CSS', description: 'Proficient' },
-    { icon: IconBrandReact, title: 'React.js', description: 'Proficient' },
-    { icon: IconBrandMantine, title: 'Mantine', description: 'Proficient' },
-    { icon: IconBrandNextjs, title: 'Next.js', description: 'Proficient' },
-    { icon: IconBrandGithub, title: 'Git', description: 'Proficient' },
-    { icon: IconBrandWordpress, title: 'WordPress', description: 'Proficient' },
-    { icon: IconBrandTypescript, title: 'TypeScript', description: 'Proficient' },
-    { icon: IconBrandDebian, title: 'GNU/Linux', description: 'Proficient' },
-    { icon: IconTerminal2, title: 'Bash', description: 'Proficient' },
-    { icon: IconBrandGoogle, title: 'Apps Script', description: 'Proficient' },
-    { icon: IconBrandGoogle, title: 'English', description: 'Native' },
-    { icon: IconBrandGoogle, title: 'Russian', description: 'Fuzzy' },
-    { icon: IconBrandGoogle, title: 'French', description: 'Clumsy' },
+    { icon: IconBrandJavascript, title: 'JavaScript' },
+    { icon: IconBrandPhp, title: 'PHP' },
+    { icon: IconBrandHtml5, title: 'HTML' },
+    { icon: IconBrandCss3, title: 'CSS' },
+    { icon: IconBrandReact, title: 'React.js' },
+    { icon: IconBrandMantine, title: 'Mantine' },
+    { icon: IconBrandNextjs, title: 'Next.js' },
+    { icon: IconBrandGithub, title: 'Git' },
+    { icon: IconBrandWordpress, title: 'WordPress' },
+    { icon: IconBrandTypescript, title: 'TypeScript' },
+    { icon: IconBrandDebian, title: 'GNU/Linux' },
+    { icon: IconTerminal2, title: 'Bash' },
+    { icon: IconBrandGoogle, title: 'Apps Script' },
+    { icon: IconBrandGoogle, title: 'English' },
+    { icon: IconBrandGoogle, title: 'Russian' },
+    { icon: IconBrandGoogle, title: 'French' },
 ];
 
 export function Skills() {
@@ -32,7 +32,6 @@ export function Skills() {
                         <div className={styles.skillContent}>
                             <skill.icon size={40} />
                             <Text fw={500} mt="md">{skill.title}</Text>
-                            <Text size="sm" color="dimmed">{skill.description}</Text>
                         </div>
 
                     </Card>
