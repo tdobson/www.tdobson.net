ab80446f50967bb241d4f7455aa333e5ba590867
Tim Dobson <hello@tdobson.net>
2024-08-13 00:20:37 +0100

    feat: Update Rates component with additional information

--- components/Rates/Rates.tsx
+++ components/Rates/Rates.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Container, Title } from '@mantine/core';
+import { Text, Container, Title, Space } from '@mantine/core';
 import classes from './Rates.module.css';
 
 export function Rates() {
@@ -8,8 +8,19 @@ export function Rates() {
       <Container size="lg">
         <Title className={classes.title}>Rates</Title>
         <Text className={classes.description}>
-            £300/day for remote work<br />
+          I'm committed to helping everyone, regardless of their financial situation. If budget is a concern, please don't hesitate to reach out - we can discuss options that work for you.
+        </Text>
+        <Space h="md" />
+        <Text className={classes.description}>
+          My standard rates are:
+        </Text>
+        <Text className={classes.description}>
+          £300/day for remote work<br />
           £400/day for onsite work within 20 miles of Stockport.
+        </Text>
+        <Space h="md" />
+        <Text className={classes.footnote}>
+          * Discounts are available for reduced payment terms, upfront payment, and longer commitments.
         </Text>
       </Container>
     </div>
