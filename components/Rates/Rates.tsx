a3b02b154261961a2fa0e6c6f0b86725d119b193
Tim Dobson <hello@tdobson.net>
2024-08-13 00:23:57 +0100

    fix: Update Rates component with revised pricing information

--- components/Rates/Rates.tsx
+++ components/Rates/Rates.tsx
@@ -7,20 +7,20 @@ export function Rates() {
     <div className={classes.wrapper}>
       <Container size="lg">
         <Title className={classes.title}>Rates</Title>
-        <Text className={classes.description}>
-          I'm committed to helping everyone, regardless of their financial situation. If budget is a concern, please don't hesitate to reach out - we can discuss options that work for you.
-        </Text>
+
         <Space h="md" />
         <Text className={classes.description}>
           My standard rates are:
         </Text>
         <Text className={classes.description}>
-          £300/day for remote work<br />
-          £400/day for onsite work within 20 miles of Stockport.
+          Remote work: £300/day for remote work<br />
+          Onsite work within 20 miles of Stockport: £400/day
         </Text>
-        <Space h="md" />
+
         <Text className={classes.footnote}>
-          * Discounts are available for reduced payment terms, upfront payment, and longer commitments.
+          *           Discounts may be available for reduced payment terms, upfront payment and longer commitments.
+
+
         </Text>
       </Container>
     </div>
