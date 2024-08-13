14818de2bda361b0cd762909b13c4619edbadb12
Tim Dobson <hello@tdobson.net>
2024-08-13 22:12:20 +0100

    feat: Adjust rate item layout on desktop

--- components/Rates/Rates.tsx
+++ components/Rates/Rates.tsx
@@ -9,20 +9,16 @@ export function Rates() {
         <Title className={classes.title}>My usual* rates are:</Title>
 
         <Space h="md" />
-        <Grid className={classes.ratesGrid}>
-          <Grid.Col span={12} sm={6}>
-            <Box className={classes.rateItem}>
-              <Text weight={700}>Remote work:</Text>
-              <Text size="xl">£300/day</Text>
-            </Box>
-          </Grid.Col>
-          <Grid.Col span={12} sm={6}>
-            <Box className={classes.rateItem}>
-              <Text weight={700}>Onsite work:**</Text>
-              <Text size="xl">£400/day</Text>
-            </Box>
-          </Grid.Col>
-        </Grid>
+        <div className={classes.ratesContainer}>
+          <Box className={classes.rateItem}>
+            <Text weight={700}>Remote work:</Text>
+            <Text size="xl">£300/day</Text>
+          </Box>
+          <Box className={classes.rateItem}>
+            <Text weight={700}>Onsite work:**</Text>
+            <Text size="xl">£400/day</Text>
+          </Box>
+        </div>
 
         <Text className={classes.footnote}>
           * Discounts may be available for reduced payment terms, upfront
